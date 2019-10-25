7eb5347b81aca3cb092baaacfd1a6c564540c686
Muhammad Ali <nerdjfpb@gmail.com>
2019-10-26 04:34:31 +0600

    added frontend pages

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AuthPage extends Component {
+  render() {
+    return(
+      <div>
+        hello there! AuthPage
+      </div>
+    )
+  }
+}
+
+export default AuthPage
---
+++ frontend/src/pages/Event.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EventPage extends Component {
+  render() {
+    return(
+      <div>
+        hello there! EventPage
+      </div>
+    )
+  }
+}
+
+export default EventPage
