9298e6e3b1ff6bfa1557d36d0aae8dcd36b2b403
Muhammad Ali <nerdjfpb@gmail.com>
2019-10-25 21:54:58 +0600

    basic query and mutation

--- app.js
+++ app.js
@@ -1,12 +1,61 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
+const events = [];
+
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send("Hello graphql")
-})
+app.use('/graphql',graphqlHttp({
+  schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
 
-app.listen(3000)
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type RootQuery {
+      events: [Event!]!
+    }
+
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return events
+    },
+    createEvent: ({ eventInput }) => {
+      const event = {
+        _id: Math.random().toString(),
+        title: eventInput.title,
+        description: eventInput.description,
+        price: eventInput.price,
+        date: new Date().toISOString()
+      }
+      events.push(event)
+      return event
+    }
+  },
+  graphiql: true
+}))
+
+app.listen(4000)
